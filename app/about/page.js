4c10ba5799c2210d209a8b4b2a5f77a4481d4979
Tezuka <vsea@ec016.just-size.net>
2025-11-04 09:52:17 +0900

    About更新

--- app/about/page.js
+++ app/about/page.js
@@ -2,8 +2,10 @@ import Container from "@/components/layout/container/container";
 import PageHero from "@/components/ui/page-hero/page-hero";
 import "@/styles/style.scss";
 import { openGraphMetadata, twitterMetadata } from "@/lib/baseMetadata";
+import profileImage from "@/images/about-profile.jpg";
 
 import { siteMeta } from "@/lib/constans";
+import Image from "next/legacy/image";
 const { siteTitle, siteUrl } = siteMeta;
 
 const pageTitle = "このサイトについて";
@@ -36,15 +38,62 @@ export default function Page() {
           image="https://images.microcms-assets.io/assets/bb6784427f6d4407af5d06e92452ab5a/546609912878437d9ca39841c03b6723/about-hero.jpg"
           title="About"
           subTitle="このサイトについて"
-          content="このサイトに関する説明です"
+          content="このサイトに関する説明です。"
         />
         <section aria-labelledby="aboutSite" className="commonSection">
           <h2 id="aboutSite" className="commonSection__heading">“空犬冒険記”とは？</h2>
-          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+          <p>ようこそ「空犬冒険記」へ！<br />このサイトでは、2023年から今までに歩いてきた長距離ウォーキングの記録を、写真と一緒に振り返っています。<br />普段よく歩くお気に入りのスポットを、（河川・湖・池や沼・海岸・市街地）の5つのカテゴリーと、（東京・神奈川・埼玉・千葉・茨城・その他）の6つのエリアに分けてご紹介。気になる場所があれば、ぜひのぞいてみてくださいね。<br />さらに「歩数算出ツール」ページでは、身長や体重、歩き方、歩くスピードを入力するだけで、1kmごとの推定歩数や時間、消費カロリーを自動で計算できます。「自分の歩き方だと、どれくらい効果があるんだろう？」と気になったときに、ちょっと試してみてもらえたら嬉しいです。</p>
         </section>
         <section aria-labelledby="profile" className="commonSection">
           <h2 id="profile" className="commonSection__heading">管理人紹介</h2>
-          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+          <div className="profile-wrapper">
+            <dl className="profile-detail">
+              <div className="profile-detail__item">
+                <dt className="profile-detail__title">名前（HN）</dt>
+                <dd className="profile-detail__body">ソライヌ</dd>
+              </div>
+              <div className="profile-detail__item">
+                <dt className="profile-detail__title">出身地</dt>
+                <dd className="profile-detail__body">静岡県富士宮市</dd>
+              </div>
+              <div className="profile-detail__item">
+                <dt className="profile-detail__title">現在地</dt>
+                <dd className="profile-detail__body">東京都中野区</dd>
+              </div>
+              <div className="profile-detail__item">
+                <dt className="profile-detail__title">誕生日</dt>
+                <dd className="profile-detail__body">11月1日</dd>
+              </div>
+              <div className="profile-detail__item">
+                <dt className="profile-detail__title">好きなスポット</dt>
+                <dd className="profile-detail__body">河川、湖、池や沼</dd>
+              </div>
+              <div className="profile-detail__item">
+                <dt className="profile-detail__title">好きなエリア</dt>
+                <dd className="profile-detail__body">埼玉、千葉</dd>
+              </div>
+              <div className="profile-detail__item">
+                <dt className="profile-detail__title">歴代ウォーキングシューズ</dt>
+                <dd className="profile-detail__body">
+                  <ul>
+                    <li>ナイキ フライニット ルナ ワン プラス ブラック/ホワイト-ライトチャコール（2013～2015年）</li>
+                    <li>ナイキ エアズームストラクチャー/ブラック（2015～2018年）</li>
+                    <li>ニューバランス フレッシュフォーム ザンテ パシュート（2019～2024年）</li>
+                    <li>アシックス ゲルカヤノ ブルー（2025年）</li>
+                  </ul>
+                </dd>
+              </div>
+            </dl>
+            <figure className="profile-image">
+              <Image
+                src={profileImage}
+                alt="プロフィール画像"
+                width="350"
+                height="350"
+                priority
+              />
+            </figure>
+          </div>
         </section>
       </Container>
     </>
--- components/layout/footer/footer.js
+++ components/layout/footer/footer.js
@@ -3,7 +3,7 @@ import styles from "./footer.module.scss";
 export default function Footer() {
   return (
     <footer role="contentinfo" className={styles.footer}>
-      <small lang="en">Copyright &copy; 2025 TOORU KATSUMATA All right reserved. </small>
+      <small lang="en">Copyright &copy; 2025 Sorainu All right reserved. </small>
     </footer>
   );
 };
